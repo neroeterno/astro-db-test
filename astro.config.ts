217114b0e9c3cc2f73d9e5acc0a86dcc692ad47e
neroeterno <102154906+neroeterno@users.noreply.github.com>
2024-06-02 12:36:49 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	site: 'https://neroeterno.github.io',
+  	base: 'astro-db-test',
 	integrations: [
 		db(),
 		tailwind({
